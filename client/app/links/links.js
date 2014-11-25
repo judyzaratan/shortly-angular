08e9db90555ad8d5c9108403a4231288268d5af7
mochicat8 <mochicat8@gmail.com>
2014-11-24 20:00:44 -0800

    Completed basic reqs

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,6 +3,7 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   // Your code here
   $scope.data = {};
+  $scope.reverse = false;
   $scope.getLinks = function(){
     Links.getLinks().then(function(data){
       $scope.data.links = data.data;
